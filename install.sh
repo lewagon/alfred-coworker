71be8b634e0aec5f0cb7a6f601a7a9bf8ea0373e
Sébastien Saunier <seb@saunier.me>
2018-02-06 15:10:11 +0100

    For install to rbenv

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 # This script is run on Hegerys by the post-receive git hook. AFTER EACH git push.
 
+export PATH="~/.rbenv/bin:${PATH}"
+eval "$(rbenv init -)"
+
 cd $1
 bundle install
 cp ~/.env/alfred-coworker/.env .
-export PATH="~/.rbenv/bin:${PATH}"
-eval "$(rbenv init -)"
 bundle exec rake -T
